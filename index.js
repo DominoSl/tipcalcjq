f58c56e0b22755982e9ec18a366e909accb3fba6
DominoSl <artemenko.mik@gmail.com>
2020-09-14 10:46:31 +0300

    jqinitfin

---
+++ index.js
@@ -0,0 +1,50 @@
+
+// calculation:
+function result() {
+    let bill = parseInt($('#bill').val(), 10);
+    let tip = parseInt($('#tip').val(), 10)* bill /100;
+    let number = $('#number').val();
+    let tipPer = tip / number;
+    let total= bill -(-tip);
+    let totalPer = total / number;
+    $('#tipf').html('$' + tipPer);
+    $('#total').html('$' + totalPer);
+} 
+
+//buttons
+
+$('#tm').click( ()=> {
+   $('#tip').val(parseInt($('#tip').val(), 10) - 1 + '%');
+   result();
+})
+
+$('#tp').click(() => {
+    $('#tip').val(parseInt($('#tip').val(), 10) + 1 + '%');
+    result();
+})
+
+$('#nm').click(() => {
+    $('#number').val(parseInt($('#number').val(), 10) - 1);
+    result();
+})
+
+$('#np').click((event) => {
+    $('#number').val(parseInt($('#number').val(), 10) + 1);
+    result();
+})
+
+//forms
+
+$('#bill').change( ()=>{
+    result();       
+})
+
+
+$('#tip').change( ()=>{
+    result();       
+})
+
+$('#number').change( ()=>{
+    result();       
+})
+
